e298a6294feab2b308372ae83a98d2387d9e9516
sammyshehter <samir_shehter@yahoo.com>
2022-12-06 18:20:08 +0200

    added cron task to start once a month

--- trigger/utils.ts
+++ trigger/utils.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
 export function reportError(errorMessage: string): void {
     console.log("ERROR!", errorMessage)
-    fs.appendFile("error.triggerApp.log", errorMessage, () => {})
+    fs.appendFile("error.triggerApp.log", `${errorMessage}\n`, () => {})
 }
